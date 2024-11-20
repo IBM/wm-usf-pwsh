b1d0132f8666e5491b077499ed0f88939d8903a3
ibmmi <175016327+ibmmi@users.noreply.github.com>
2024-11-20 16:24:59 +0100

    More refinement on the devcontainer as I expand the usage
    Signed-off-by: ibmmi <175016327+ibmmi@users.noreply.github.com>

--- 01.code/wm-usf-common.psm1
+++ 01.code/wm-usf-common.psm1
@@ -4,3 +4,18 @@ function Invoke-EnvironmentSubstitution() {
   Get-ChildItem Env: | Set-Variable
   $ExecutionContext.InvokeCommand.ExpandString($InputObject)
 }
+
+function Get-NewTempDir() {
+  param (
+    # log message
+    [Parameter(Mandatory = $false)]
+    [string]${tmpBaseDir} = ${env:TEMP}
+  )
+
+  if ( ${tmpBaseDir}.Substring(${tmpBaseDir}.Length - 1, 1) -ne [IO.Path]::DirectorySeparatorChar ) {
+    ${tmpBaseDir} += [IO.Path]::DirectorySeparatorChar
+  }
+
+  $r = $tmpBaseDir + (Get-Date -UFormat "%y%m%d%R" | ForEach-Object { $_ -replace ":", "." })
+  return $r
+}
--- 02.test/wm-us-common_test.ps1
+++ 02.test/wm-us-common_test.ps1
@@ -1,24 +1,20 @@
 Import-Module "$PSScriptRoot/../01.code/wm-usf-common.psm1" -Force
 
-function checkPester(){
+function checkPester() {
   $pesterModules = @( Get-Module -Name "Pester" -ErrorAction "SilentlyContinue" );
-  if( ($null -eq $pesterModules) -or ($pesterModules.Length -eq 0) )
-  {
+  if ( ($null -eq $pesterModules) -or ($pesterModules.Length -eq 0) ) {
     Import-Module -Name Pester -RequiredVersion ${env:PESTER_VERSION}
     $pesterModules = @( Get-Module -Name "Pester" -ErrorAction "SilentlyContinue" );
-    if( ($null -eq $pesterModules) -or ($pesterModules.Length -eq 0) )
-    {
-        throw "no pester module loaded!";
+    if ( ($null -eq $pesterModules) -or ($pesterModules.Length -eq 0) ) {
+      throw "no pester module loaded!";
     }
   }
 
-  if( $pesterModules.Length -gt 1 )
-  {
-      throw "multiple pester modules loaded!";
+  if ( $pesterModules.Length -gt 1 ) {
+    throw "multiple pester modules loaded!";
   }
-  if( $pesterModules[0].Version -ne ([version] "${env:PESTER_VERSION}") )
-  {
-      throw "unsupported pester version '$($pesterModules[0].Version)'";
+  if ( $pesterModules[0].Version -ne ([version] "${env:PESTER_VERSION}") ) {
+    throw "unsupported pester version '$($pesterModules[0].Version)'";
   }
   Write-Output "Pester module OK"
 }
@@ -26,15 +22,36 @@ checkPester || exit 1 # Cannot continue if pester setup is incorrect
 
 Describe "Basics" {
   Context 'Environment Substitutions' {
-    It 'Substitutes env vars'{
+    It 'Substitutes env vars' {
       $inString = 'aa ${env:b} cc'
       $env:b = 'B'
       $inString | Invoke-EnvironmentSubstitution | Should -Be 'aa B cc'
     }
-    It 'Substitutes absent vars'{
+    It 'Substitutes absent vars' {
       $inString = 'aa ${env:b} cc'
       $env:b = $null
       $inString | Invoke-EnvironmentSubstitution | Should -Be 'aa  cc'
+    }
+  }
+
+  Context 'Temp Directories' {
+    It 'Check unix trailing slashes passed' {
+      ${newTmpDir} = $(Get-NewTempDir("/tmp/"))
+      ${newTmpDir}.Substring(0, 6) | Should -Not -Be "/tmp//"
+
+    }
+    It 'Check unix trailing slashes not passed' {
+      ${newTmpDir} = $(Get-NewTempDir("/tmp"))
+      ${newTmpDir}.Substring(0, 5) | Should -Be "/tmp/"
+
+    }
+    It 'Create and destroy new temp dir' {
+      ${newTmpDir} = $(Get-NewTempDir("/tmp"))
+      Test-Path ${newTmpDir} | Should -be $false
+      New-Item ${newTmpDir} -Force
+      Test-Path ${newTmpDir} | Should -be $true
+      Remove-Item ${newTmpDir}
+      Test-Path ${newTmpDir} | Should -be $false
     }
   }
 }
