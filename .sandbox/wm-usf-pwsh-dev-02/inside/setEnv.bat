6065412e7034f3dfdd98581882efb8868f9c5335
ibmmi <175016327+ibmmi@users.noreply.github.com>
2025-03-20 09:32:58 +0100

    bumped dependency versions to latest as of today

--- .sandbox/wm-usf-pwsh-dev-02/inside/setEnv.bat
+++ .sandbox/wm-usf-pwsh-dev-02/inside/setEnv.bat
@@ -1,5 +1,5 @@
-SET POWERSHELL_VERSION=7.4.6
-SET PESTER_VERSION=5.6.1
+SET POWERSHELL_VERSION=7.5.0
+SET PESTER_VERSION=5.7.1
 
 :: Powershell doenload URL
 SET PU=https://github.com/PowerShell/PowerShell/releases/download/v%POWERSHELL_VERSION%/PowerShell-%POWERSHELL_VERSION%-win-x64.msi
